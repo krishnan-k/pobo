4b2c36b753ed5ad753e877629ddd5ffa8473cf8a
krishnan <krishnank281099@gmail.com>
2025-02-23 22:55:10 +0530

    breadcrumb

--- wireframe/src/component/Breadcrumb.jsx
+++ wireframe/src/component/Breadcrumb.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Breadcrumb = () => {
   return (
-    <div className='breadcrumb_section bg-[#e2e8f0] pt-11 pb-12'>
+    <div className='breadcrumb_section bg-[#e2e8f0] pt-11 pb-12 max-[767px]:pt-8 max-[767px]:pb-9'>
       <h1 className='text-center text-[40px] font-semibold text-[#272b41] max-[767px]:text-[25px]'>Doctor Request</h1>
       <div className='redirect_page_link text-center'>
         <span className='link_1 capitalize text-[13px] text-[#4b5f95]'>home</span>
